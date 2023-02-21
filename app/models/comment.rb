b32ff3fc3530b6f478ebc59f9c24d7fc62f4a0d7
Ben Sales <adebsa2401@gmail.com>
2023-02-21 04:24:28 +0100

    feat: Add Active record callbacks on counters update methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,10 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
+  after_save :update_comments_counter
+
+  private
+
   def update_comments_counter
     post.update(comments_counter: post.comments.count)
   end
--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,10 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
+  after_save :update_likes_counter
+
+  private
+
   def update_likes_counter
     post.update(likes_counter: post.likes.count)
   end
