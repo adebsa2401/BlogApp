93e8ade89d2ebdbb86d584980649cce0470a62c7
Ben Sales <adebsa2401@gmail.com>
2023-02-21 03:14:22 +0100

    feat: Add some custom methods to the models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,12 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
+
+  def update_posts_counter
+    author.update(posts_counter: author.posts.count)
+  end
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
