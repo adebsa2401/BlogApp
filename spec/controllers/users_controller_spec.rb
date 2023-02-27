7c669bb6953e43bad391df066d3666ce37e0733a
Ben Sales <adebsa2401@gmail.com>
2023-02-27 15:31:48 +0100

    spec: Add request spec for users controller

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :request do
+  describe 'GET #index' do
+    it 'returns http success' do
+      get users_path
+      expect(response).to be_successful
+      expect(response.body).to include('List of users')
+    end
+  end
+
+  describe 'GET #show' do
+    let :user do
+      User.create do |user|
+        user.name = 'John Doe'
+      end
+    end
+
+    it 'returns http success' do
+      get user_path(user)
+      expect(response).to be_successful
+      expect(response.body).to include('User details')
+    end
+  end
+end
